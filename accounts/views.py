b70330dde58863b8d11062d231db9460b6f0c578
Juan Yunis <juanyunis@gmail.com>
2011-04-20 09:24:31 -0500

    Update accounts handling

--- accounts/views.py
+++ accounts/views.py
@@ -7,17 +7,16 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.core.urlresolvers import reverse
 from django_open_inviter.open_inviter import OpenInviter
 from contacts.models import Contact
+from errors.views import general_error
 
 @login_required
 def user_settings(request):
-
-    
     return render_to_response('accounts/profile.html', {'accounts_active':'active', 'user':request.user },
                           context_instance=RequestContext(request))
 
 def user_login(request):
-    if request.method == 'POST':
-        try:
+    try:
+        if request.method == 'POST':
             form = AuthenticationForm(data=request.POST)
 
             if form.is_valid():
@@ -26,17 +25,17 @@ def user_login(request):
                     login(request, user)
                     return HttpResponseRedirect(request.POST['next'])
                 else:
-                    return HttpResponseRedirect(reverse('home'))
+                    return general_error(request, title='Error Login', message='Invalid Authentication', redirect='accounts')
             else:
-                return HttpResponseRedirect(reverse('home'))
-        except:
-            from django.conf.urls.defaults import handler500
-            return HttpResponseRedirect(handler500)
-    else:
-        form = AuthenticationForm()
-        
-        return render_to_response('accounts/login.html', {'accounts_active':'active', 'form':form},
-                              context_instance=RequestContext(request))
+                return general_error(request, title='Error Login', message='Invalid Authentication Form', redirect='accounts')
+        else:
+            form = AuthenticationForm()
+
+            return render_to_response('accounts/login.html', {'accounts_active':'active', 'form':form},
+                                  context_instance=RequestContext(request))
+    except Exception, e:
+        return general_error(request, title='Error Login', message=e.message, redirect='accounts')
+
 @login_required
 def user_logout(request):
     if request.user is not None:
@@ -45,27 +44,29 @@ def user_logout(request):
     return HttpResponseRedirect(reverse('home'))
 
 def user_register(request):
-    if request.method == 'POST':
-        user = User.objects.create_user(request.POST['username'], '', request.POST['password1'])
-        user.save()
+    try:
+        if request.method == 'POST':
+            user = User.objects.create_user(request.POST['username'], '', request.POST['password1'])
+            user.save()
 
-        user = authenticate(username=request.POST['username'], password=request.POST['password1'])
-        login(request, user)
-        
-        return HttpResponseRedirect(reverse('dashboard'))
-    else:
-        form = UserCreationForm()
-        return render_to_response('accounts/register.html', {'accounts_active':'active', 'form':form},
-                              context_instance=RequestContext(request))
+            user = authenticate(username=request.POST['username'], password=request.POST['password1'])
+            login(request, user)
+
+            return HttpResponseRedirect(reverse('dashboard'))
+        else:
+            form = UserCreationForm()
+            return render_to_response('accounts/register.html', {'accounts_active':'active', 'form':form},
+                                  context_instance=RequestContext(request))
+    except Exception, e:
+        return general_error(request, title='Error Registering', message=e.message, redirect='accounts')
 
 @login_required
 def import_contacts(request):
-    if request.method == 'POST':
-        try:
+    try:
+        if request.method == 'POST':
             form = AuthenticationForm(data=request.POST)
 
             o = OpenInviter()
-
             contacts = o.contacts(request.POST['username'], request.POST['password'])
 
             total = len(contacts)
@@ -75,17 +76,14 @@ def import_contacts(request):
                                         context_instance=RequestContext(request))
             else:
                 return HttpResponseRedirect(reverse('accounts'))
-        except:
-            #from django.conf.urls.defaults import handler500
-            #return HttpResponseRedirect(reverse('errors.views.general_error'))
-            from errors.views import general_error
-            
-            return general_error(request, title='Error Importing', message='Invalid credentials', redirect='accounts')
-    else:
-        form = AuthenticationForm()
+        else:
+            form = AuthenticationForm()
 
-        return render_to_response('accounts/import.html', {'accounts_active':'active', 'form':form, },
-                                    context_instance=RequestContext(request))
+            return render_to_response('accounts/import.html', {'accounts_active':'active', 'form':form, },
+                                        context_instance=RequestContext(request))
+    except Exception, e:
+        return general_error(request, title='Error Importing Contacts', message=e.message, redirect='accounts')
+    
 @login_required
 def update_contacts(request):
     if request.method == 'POST':
@@ -98,7 +96,7 @@ def update_contacts(request):
                 c.email = request.POST['email' + str(i)]
 
                 c.save()
-            except:
+            except Exception:
                 print 'none'
 
         return HttpResponseRedirect(reverse('dashboard'))
